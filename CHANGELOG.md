96792bd8787d455de71900e4a2c5d126dc60a175
yang qingwu <yang qingwu>
2016-06-03 15:27:56 +0800

    提交composer和PHP的源文件

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+
+All Notable changes to `helloworld` will be documented in this file.
+
+Updates should follow the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
+
+## NEXT - 2016-6-3
+
+### Added
+- Nothing
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Nothing
+
+### Removed
+- Nothing
+
+### Security
+- Nothing
---
+++ src/HelloWorld.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace YunHack\HelloWorld;
+
+class HelloWorld
+{
+    /**
+     * Create a new HelloWorld Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * return a string,"hello world!"
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoHelloWorld()
+    {
+        return "hello world!";
+    }
+}
